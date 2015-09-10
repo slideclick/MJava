38b2d8f7ccea0163738c8279bd618086a5b375e5
unknown <lius@CPEKW-Q1100534.is.ad.igt.com>
2015-09-10 11:26:52 +0800

    can't compile because weiss have no toString function

--- learnPakcage/helloArray.java
+++ learnPakcage/helloArray.java
@@ -1,9 +1,26 @@
+//import ds.util.Arrays;
+import weiss.util.Arrays;
+class MyClass{
+private int i ;
+public MyClass(int ii){
+    this.i = ii;
+}
+
+}
 
 public class helloArray {
 
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        
+        MyClass a[] = new MyClass[10];
+        for (int j  = 0; j< 10; j++){
+            a[j] = new MyClass(j);
+        }
+        
+        System.out.println(Arrays.toString(a));
+        
 	}
 
 }
